992f1bf68c9305f6b164b28dfaa1a554267ee5a5
dan <dan.leash@siegemarkets.com>
2024-04-30 15:29:37 -0700

    check

--- MauiApp2/obj/Debug/net8.0-ios/iossimulator-arm64/MauiApp2.AssemblyInfo.cs
+++ MauiApp2/obj/Debug/net8.0-ios/iossimulator-arm64/MauiApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+be7412ef726893e4e6903d831d6b3d7ab6a5b38d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f791e49d5ce5eb5ce13de66b4c62fc5ebbdf7d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RazorClassLibrary2/obj/Debug/net8.0/RazorClassLibrary2.AssemblyInfo.cs
+++ RazorClassLibrary2/obj/Debug/net8.0/RazorClassLibrary2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorClassLibrary2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be7412ef726893e4e6903d831d6b3d7ab6a5b38d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f791e49d5ce5eb5ce13de66b4c62fc5ebbdf7d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorClassLibrary2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorClassLibrary2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
